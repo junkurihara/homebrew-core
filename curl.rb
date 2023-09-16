3fe9403d1b7cfc8c5c06e80a4353aadab55d66fc
Jun Kurihara <junkurihara@users.noreply.github.com>
2023-09-16 17:19:44 +0900

    downgrade curl

--- curl.rb
+++ curl.rb
@@ -12,11 +12,11 @@
 class Curl < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server with HTTP/3 support using quiche"
   homepage "https://curl.se"
-  url "https://curl.se/download/curl-8.3.0.tar.bz2"
-  mirror "https://github.com/curl/curl/releases/download/curl-8_3_0/curl-8.3.0.tar.bz2"
-  mirror "http://fresh-center.net/linux/www/curl-8.3.0.tar.bz2"
-  mirror "http://fresh-center.net/linux/www/legacy/curl-8.3.0.tar.bz2"
-  sha256 "051a217095671e925a129ba9e2ff2e223b44b08399003ba50738060955d010ff"
+  url "https://curl.se/download/curl-8.2.1.tar.bz2"
+  mirror "https://github.com/curl/curl/releases/download/curl-8.2.1/curl-8.2.1.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/curl-8.2.1.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/legacy/curl-8.2.1.tar.bz2"
+  sha256 "0f1e31ebe336c09ec66381f1532f8350e466e1d02ffe10c4ac44a867f1b9d343"
   license "curl"
 
   livecheck do
